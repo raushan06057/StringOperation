0d47cd0458c01efe9942ea806a35184a4472ffe5
RAUSHAN SINGH <raushanaria184@outlook.com>
2024-08-17 23:46:29 +0530

    Reverse the order of word of String using C#

---
+++ ReverseStringWord/ReverseStringWord/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter the String ");
+string input = Console.ReadLine();
+if(!string.IsNullOrEmpty(input))
+{
+    string[] strArray = input.Split(' ');
+    if(strArray.Length > 0)
+    {
+        for(int counter= strArray.Length-1; counter>=0; counter--)
+        {
+            Console.WriteLine(strArray[counter]);
+        }
+    }
+}
